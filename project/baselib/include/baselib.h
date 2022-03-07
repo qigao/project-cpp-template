a5fbacbe16a25b184494eed044c7522298275402
Qi Gao <qi.gao2012@gmail.com>
2022-03-07 12:14:29 +0800

    feat(baselib): add c style dll template (#4)
    * feat(baselib): add c style dll template

---
+++ project/baselib/include/baselib.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+
+#include <string>
+
+#include <baselib_export.h>
+
+
+namespace baselib
+{
+
+
+/**
+*  @brief
+*    Print information about the library to the console
+*/
+BASELIB_EXPORT void printInfo();
+
+
+} // namespace baselib
--- project/helloapp/src/helloapp.cpp
+++ project/helloapp/src/helloapp.cpp
@@ -1,18 +1,12 @@
-#include "git.h"
+
 #include "hellolib.h"
 #include <iostream>
 using namespace hello;
-
+#include "baselib.h"
 int main()
 {
 
-  std::cout << "Version: " << PROJECT_VERSION << "\n"
-            << "Commit: " << GIT_DESCRIBE << "\n"
-            << "Author: " << GIT_AUTHOR_NAME << " <" << GIT_AUTHOR_EMAIL << ">\n"
-            << "State: " << (GIT_IS_DIRTY ? "dirty" : "") << "\n"
-            << "Date: " << GIT_COMMIT_DATE_ISO8601 << "\n\n"
-            << GIT_COMMIT_SUBJECT << "\n"
-            << GIT_COMMIT_BODY << std::endl;
+
 
   hellolib hello{};
   int32_t error_code = hello.saySomething("Hello Modern C++ Development");
@@ -21,5 +15,6 @@ int main()
   error_code = hello.saySomethingHashed("Hello Modern C++ Development");
   if (error_code > 0) { return error_code; }
 #endif
+  baselib::printInfo();
   return 0;
 }
