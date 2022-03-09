e0e14fd9fd45b29057f1f646e09351012ee4b658
Qi Gao <qi.gao2012@gmail.com>
2022-03-09 15:27:34 +0800

    chore(cona): cmake-conan - [x] use conanfile.txt - [x] add module path to dependencies.cmake - [x] chore(actions): drop conan env

--- cmake/conan.cmake
+++ cmake/conan.cmake
@@ -4,28 +4,16 @@ if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
       "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
   file(
     DOWNLOAD
-    "https://raw.githubusercontent.com/conan-io/cmake-conan/release/0.17/conan.cmake"
+    "https://raw.githubusercontent.com/conan-io/cmake-conan/0.17.0/conan.cmake"
     "${CMAKE_BINARY_DIR}/conan.cmake"
+    EXPECTED_HASH
+      SHA256=3bef79da16c2e031dc429e1dac87a08b9226418b300ce004cc125a82687baeef
     TLS_VERIFY ON)
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
+conan_cmake_run(CONANFILE conanfile.txt
+                BASIC_SETUP CMAKE_TARGETS
+                INSTALL_FOLDER ${CMAKE_INSTALL_PREFIX}
+                BUILD missing)
 
-conan_cmake_configure(REQUIRES fmt/8.1.1 GENERATORS cmake_find_package)
-conan_cmake_configure(REQUIRES spdlog/1.9.2 GENERATORS cmake_find_package)
-conan_cmake_configure(REQUIRES docopt.cpp/0.6.3 GENERATORS cmake_find_package)
-conan_cmake_configure(REQUIRES catch2/2.13.8 GENERATORS cmake_find_package)
-conan_cmake_configure(REQUIRES doctest/2.4.6 GENERATORS cmake_find_package)
-conan_cmake_configure(REQUIRES openssl/1.1.1l GENERATORS cmake_find_package)
-
-conan_cmake_autodetect(settings)
-
-conan_cmake_install(
-  PATH_OR_REFERENCE
-  .
-  BUILD
-  missing
-  REMOTE
-  conancenter
-  SETTINGS
-  ${settings})
