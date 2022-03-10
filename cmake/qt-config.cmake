89fe7b61811ae5550996248459d4f5ec469a3e1e
Qi Gao <qi.gao2012@gmail.com>
2022-03-10 09:05:39 +0800

    feat(qt): add resource to gui app (#13)
    * feat(qt): add file icon and version info
    - [x] update cmake var
    - [x] add file icon to win

---
+++ cmake/qt-config.cmake
@@ -0,0 +1,14 @@
+macro(add_project_meta FILES_TO_INCLUDE)
+  if(APPLE)
+    set(ICON_FILE ${RESOURCE_FOLDER}/${ICON_NAME}.icns)
+  elseif(WIN32)
+    set(ICON_FILE ${RESOURCE_FOLDER}/${ICON_NAME}.ico)
+  endif()
+
+  if(WIN32)
+    configure_file(
+      "${PROJECT_SOURCE_DIR}/templates/qt/resources/windows_metafile.rc.in"
+      "windows_metafile.rc")
+    set(RES_FILES "windows_metafile.rc")
+  endif()
+endmacro()
