b8b4695c4b4ea2a5018d452aa1466a09eb2e886e
Qi Gao <qi.gao2012@gmail.com>
2022-03-12 11:29:12 +0800

    fmt(qapp): fmt code

--- project/examples/Qapp/main.cpp
+++ project/examples/Qapp/main.cpp
@@ -1,27 +1,30 @@
+#include "version.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include "version.h"
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
-    QGuiApplication app(argc, argv);
-    app.setApplicationName(QString::fromLocal8Bit(APPLICATION_NAME));
-    app.setApplicationVersion(QString::fromLocal8Bit(APPLICATION_VERSION_STRING_FULL));
-    app.setOrganizationName(QString::fromLocal8Bit(APPLICATION_COMPANY_NAME));
-    app.setOrganizationDomain(QString::fromLocal8Bit(APPLICATION_COMPANY_DOMAIN));
+  QGuiApplication app(argc, argv);
+  app.setApplicationName(QString::fromLocal8Bit(APPLICATION_NAME));
+  app.setApplicationVersion(QString::fromLocal8Bit(APPLICATION_VERSION_STRING_FULL));
+  app.setOrganizationName(QString::fromLocal8Bit(APPLICATION_COMPANY_NAME));
+  app.setOrganizationDomain(QString::fromLocal8Bit(APPLICATION_COMPANY_DOMAIN));
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+  QQmlApplicationEngine engine;
+  const QUrl url(QStringLiteral("qrc:/main.qml"));
+  QObject::connect(
+    &engine,
+    &QQmlApplicationEngine::objectCreated,
+    &app,
+    [url](QObject *obj, const QUrl &objUrl) {
+      if (!obj && url == objUrl) QCoreApplication::exit(-1);
+    },
+    Qt::QueuedConnection);
+  engine.load(url);
 
-    return app.exec();
+  return app.exec();
 }
