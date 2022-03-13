7791e64dd1b913f749ec0451d0a0691aaf2c3699
Qi Gao <qi.gao2012@gmail.com>
2022-03-14 00:21:08 +0800

    feat(hello): add cmd line args - [x] add deps cxxopts - [x] cxxopts demo in helloapp - [x]  fix qapp for missing  check git - [x] update PR template

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -21,52 +21,17 @@ Ex:
     * what was the bug/error (if you already made an issue, please link to it here)
     * how does your implementation fix the issue
 
-### Code style and formatting
-
-Check the [Contributors Style Guidelines section](CONTRIBUTING.md#Style-guidelines)
-for how to write your code and the [Contributors Code Formatting section](CONTRIBUTING.md#Code-formatting)
-for how to format your code.
-
 #### Closing Issues
 
-Put `closes #XXXX` in your comment to auto-close the issue that your PR fixes
+Put `closes #XXX` in your comment to auto-close the issue that your PR fixes
 (if such).
 
 ---
 
-Fixes #XXXX
+Fixes #XXX
 
 ## Proposed changes
 
-*
-*
-*
+- []
+- []
 
-## Motivation behind changes
-
-### Test plan
-
-Demonstrate the code is solid. Example: The exact commands you ran and their output,
-screenshots / videos if the pull request changes UI.
-
-*Make sure tests pass on all of the [relevant CI workflows](https://github.com/filipdutescu/modern-cpp-template/actions).*
-
-### Pull Request Readiness Checklist
-
-See details at [CONTRIBUTING.md](https://github.com/filipdutescu/modern-cpp-template/blob/master/CONTRIBUTING.md).
-
-* [ ] I agree to contribute to the project under [INSERT PROJECT NAME] (Unlicense)
-[License](LICENSE).
-
-* [ ] To the best of my knowledge, the proposed patch is not based on a code under
-GPL or other license that is incompatible with [INSERT PROJECT NAME]
-
-* [ ] The PR is proposed to proper branch
-
-* [ ] There is reference to original bug report and related work
-
-* [ ] There is accuracy test, performance test and test data in the repository,
-if applicable
-
-* [ ] The feature is well documented and sample code can be built with the project
-CMake
--- project/examples/helloapp/src/helloapp.cpp
+++ project/examples/helloapp/src/helloapp.cpp
@@ -1,10 +1,47 @@
 
-#include "hellolib.h"
+#include <cxxopts.hpp>
 #include <iostream>
-using namespace hello;
+
 #include "baselib.h"
-int main()
+#include "hellolib.h"
+using namespace hello;
+int main(int argc, char **argv)
 {
+  cxxopts::Options options("test", "A brief description");
+  // clang-format off
+  options
+    .set_width(70)
+    .set_tab_expansion()
+    .allow_unrecognised_options()
+    .add_options()
+      ("b,bar", "Param bar", cxxopts::value<std::string>())
+      ("d,debug", "Enable debugging", cxxopts::value<bool>()->default_value("false"))
+      ("f,foo", "Param foo", cxxopts::value<int>()->default_value("10"))
+      ("h,help", "Print usage");
+  // clang-format on
+  auto result = options.parse(argc, argv);
+  auto &unmatched = result.unmatched();// get the unmatched arguments
+  if (unmatched.size() > 0) {
+    std::cout << "unmatched: " << unmatched.size() << std::endl;
+    for (auto &u : unmatched) { std::cout << u << std::endl; }
+  }
+  if (result.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  }
+  bool debug = result["debug"].as<bool>();
+  if (debug) {
+    std::cout << "Debug mode is enabled" << std::endl;
+    baselib::printInfo();
+  }
+  std::string bar;
+  if (result.count("bar")) {
+    bar = result["bar"].as<std::string>();
+    std::cout << "bar: " << bar << std::endl;
+  }
+
+  int foo = result["foo"].as<int>();
+  std::cout << "foo: " << foo << std::endl;
   hellolib hello{};
   int32_t error_code = hello.saySomething("Hello Modern C++ Development");
   if (error_code > 0) { return error_code; }
@@ -12,6 +49,5 @@ int main()
   error_code = hello.saySomethingHashed("Hello Modern C++ Development");
   if (error_code > 0) { return error_code; }
 #endif
-  baselib::printInfo();
   return 0;
 }
