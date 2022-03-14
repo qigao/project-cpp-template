c080a2492391fed437e394d971946c8d7f037f6e
Qi Gao <qi.gao2012@gmail.com>
2022-03-14 10:20:14 +0800

    fmt(cmake): fmt cmake code (#34)

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,9 +1,3 @@
-# MIT License
-# Copyright (c) 2018-Today Michele Adduci <adduci@tutanota.com>
-#
-# Dependencies
-
-
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_INSTALL_PREFIX})
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
 # Required for Testing
