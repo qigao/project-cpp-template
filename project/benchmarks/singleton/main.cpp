d22ea977af044f3bc875a1b0738276548a67e6f8
Qi Gao <qi.gao2012@gmail.com>
2022-03-26 21:31:59 +0800

    - [x] add thread support by cmake (#62)
    - [x] drop tidy,cppcheck from cmake task
    - [x] drop gateway directory

--- project/benchmarks/singleton/main.cpp
+++ project/benchmarks/singleton/main.cpp
@@ -7,17 +7,10 @@
 class SingletonDemo : public Singleton<SingletonDemo>
 {
 };
-static void ThreadBarWithMutex()
-{
-  auto singleton = SingletonDemo::GetInstance("Bar");
-}
-static void ThreadBarWithFlag()
-{
-  auto singleton = SingletonDemo::get("Bar");
-}
+
 static void BM_SingletonWithMutex(benchmark::State &state)
 {
-  for (auto _ : state) { ThreadBarWithMutex(); }
+  for (auto _ : state) { auto singleton = SingletonDemo::GetInstance("Bar"); }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_SingletonWithMutex)->Range(1 << 0, 1 << 10);
@@ -25,7 +18,7 @@ BENCHMARK(BM_SingletonWithMutex)->Range(1 << 0, 1 << 10);
 // Define another benchmark
 static void BM_SingletonWithFlag(benchmark::State &state)
 {
-  for (auto _ : state) ThreadBarWithFlag();
+  for (auto _ : state) { auto singleton = SingletonDemo::get("Bar"); }
 }
 BENCHMARK(BM_SingletonWithFlag)->Range(1 << 0, 1 << 10);
 
