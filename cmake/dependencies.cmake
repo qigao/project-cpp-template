052de1411610559c1e3cc2998f2f5cfbaf051a8a
Qi Gao <qi.gao2012@gmail.com>
2022-04-08 22:02:03 +0800

    fix(cmake): update dependencies.cmake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,10 +1,16 @@
 # Required for Testing
 if(BUILD_TESTING)
-  find_package(doctest REQUIRED)
+  find_package(Gtest REQUIRED)
 endif()
-if (BUILD_BENCHMARKS)
+
+if(BUILD_BENCHMARKS)
   find_package(benchmark REQUIRED)
-endif ()
+endif()
+
 # Optional Dependency, doesn't trigger error if missing
 find_package(OpenSSL)
 find_package(cxxopts)
+find_package(Threads REQUIRED)
+find_package(uvw CONFIG REQUIRED)
+find_package(readerwriterqueue REQUIRED)
+find_package(concurrentqueue REQUIRED)
