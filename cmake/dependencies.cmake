0c918b3c427abb82f790b24cf84c7d654d09e683
Qi Gao <qi.gao2012@gmail.com>
2023-04-18 22:58:17 +0800

    Chore-doc (#83)

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -2,14 +2,10 @@
 if(BUILD_TESTING)
   find_package(GTest REQUIRED)
 endif()
-
-if(BUILD_BENCHMARKS)
-  find_package(benchmark REQUIRED)
-endif()
+find_package(benchmark)
 
 # Optional Dependency, doesn't trigger error if missing
 find_package(cxxopts)
 find_package(Threads REQUIRED)
-find_package(uvw REQUIRED)
 find_package(readerwriterqueue REQUIRED)
 find_package(concurrentqueue REQUIRED)
