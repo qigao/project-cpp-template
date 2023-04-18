38e5e24b79fec597226152dd9c9a6147de24e70d
Qi Gao <qi.gao2012@gmail.com>
2023-04-18 14:41:43 +0800

    drop vcpkg

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,15 +1,11 @@
 # Required for Testing
 if(BUILD_TESTING)
   find_package(GTest REQUIRED)
-endif()
-
-if(BUILD_BENCHMARKS)
-  find_package(benchmark REQUIRED)
+  find_package(benchmark CONFIG REQUIRED)
 endif()
 
 # Optional Dependency, doesn't trigger error if missing
 find_package(cxxopts)
 find_package(Threads REQUIRED)
-find_package(uvw REQUIRED)
 find_package(readerwriterqueue REQUIRED)
 find_package(concurrentqueue REQUIRED)
